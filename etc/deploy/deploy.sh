167f8e55a8340ea09752abd4e2d4484873680f05
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2020-04-17 10:57:27 +0100

    EDATOS-3146 - Instalación del entorno de estadísticas en la nueva máquina OVH
    > Correcciones de errores detectados en el primer despliegue

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -5,6 +5,7 @@ TRANSFER_PATH=$HOME_PATH/tmp
 DEMO_ENV=$HOME_PATH/env
 DEPLOY_TARGET_PATH_EXTERNAL=/servers/edatos-external/tomcats/edatos-external01/webapps
 DEPLOY_TARGET_PATH_INTERNAL=/servers/edatos-internal/tomcats/edatos-internal01/webapps
+ENVIRONMENT_RELATIVE_PATH_FILE=WEB-INF/classes/application.properties
 
 RESTART=1
 
@@ -35,8 +36,8 @@ ssh -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" depl
     sudo rm -rf $DEPLOY_TARGET_PATH_INTERNAL/complementos-apps.war
     
     # Restore Configuration
-    sudo rm -f $DEPLOY_TARGET_PATH_INTERNAL/complementos-apps/WEB-INF/classes/application.properties
-    sudo cp $DEMO_ENV/application_internal.properties $DEPLOY_TARGET_PATH_INTERNAL/complementos-apps/WEB-INF/classes/
+    sudo rm -f $DEPLOY_TARGET_PATH_INTERNAL/complementos-apps/$ENVIRONMENT_RELATIVE_PATH_FILE
+    sudo cp $DEMO_ENV/application_internal.properties $DEPLOY_TARGET_PATH_INTERNAL/complementos-apps/$ENVIRONMENT_RELATIVE_PATH_FILE
 
     if [ $RESTART -eq 1 ]; then
         sudo chown -R edatos-internal.edatos-internal /servers/edatos-internal     
@@ -59,8 +60,8 @@ ssh -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" depl
     sudo rm -rf $DEPLOY_TARGET_PATH_EXTERNAL/complementos-apps.war
     
     # Restore Configuration
-    sudo rm -f $DEPLOY_TARGET_PATH_EXTERNAL/complementos-apps/WEB-INF/classes/application.properties
-    sudo cp $DEMO_ENV/application_external.properties $DEPLOY_TARGET_PATH_EXTERNAL/complementos-apps/WEB-INF/classes/
+    sudo rm -f $DEPLOY_TARGET_PATH_EXTERNAL/complementos-apps/$ENVIRONMENT_RELATIVE_PATH_FILE
+    sudo cp $DEMO_ENV/application_external.properties $DEPLOY_TARGET_PATH_EXTERNAL/complementos-apps/$ENVIRONMENT_RELATIVE_PATH_FILE
 
     if [ $RESTART -eq 1 ]; then
         sudo chown -R edatos-external.edatos-external /servers/edatos-external        
