0d649e0ae3f739b93d119b4044f97b9d6bd8d3d2
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2020-08-10 15:29:20 +0100

    EDATOS-3190 - Actualizar los estilos del visualizador con los nuevos de gobierno
    > Añadidos más recursos

--- scripts/update-external-resources.js
+++ scripts/update-external-resources.js
@@ -11,6 +11,10 @@ const CSS_FILE_LIST = [
     { path: "gcc/css/cuerpo.css" },
     { path: "gcc/css/imprime.css", media: "print" },
     { path: "gcc/css/voz.css", media: "aural" },
+    { path: "cmsweb/export/system/modules/es.gobcan.portal.tipo/resources/css/tipo_cabecera.css" },
+    { path: "cmsweb/export/system/modules/es.gobcan.portal.tipo/resources/css/tipo_redes_sociales.css" },
+    { path: "cmsweb/export/system/modules/es.gobcan.portal.tipo/resources/css/tipo_widget.css" },
+    { path: "cmsweb/export/system/modules/es.gobcan.portal.tipo/resources/css/tipo_caja_lateral.css" },
     { path: "gcc/css/responsive/bootstrap.min.css" },
     { path: "gcc/css/responsive/custom_responsive.css" },
     { path: "gcc/css/responsive/custom_bootstrap.css" },
@@ -27,8 +31,13 @@ const RESOURCES_FILE_LIST = [
     { path: "gcc/img/iconos/rss_20x20.png" },
     { path: "gcc/img/iconos/twitter_20x20.png" },
     { path: "gcc/img/iconos/Slideshare_20x20.png" },
-    { path: "gcc/img/iconos/youtube_20x20.png" }
+    { path: "gcc/img/iconos/youtube_20x20.png" },
+    { path: "istac/resources/imagenes/logo_istac.jpg" },
+    { path: "gcc/img/maqueta/borde_bajo_migas.gif" },
+    { path: "gcc/img/auxis/flecha_amarilla.gif" },
+    { path: "gcc/img/listas/flecha_menulat.gif" },
 ];
+
 
 RESOURCES_FILE_LIST.forEach((file) => {
     mkdirp.sync(path.dirname(BASE_FOLDER + BASE_PATH + file.path));
