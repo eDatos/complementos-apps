4ce8263f854eada7e4c9de91b74b786d10f7387c
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2020-11-24 10:49:36 +0000

    EDATOS-3215 - Refactorización del código para que la propiedad organización sea escalable
    > Movidos /apps/apis/ a  /organisations/istac/apis/header/

--- etc/changes-from-release/2.1.1/db/common-metadata/oracle/20201123_update_paths_after_refactor.sql
+++ etc/changes-from-release/2.1.1/db/common-metadata/oracle/20201123_update_paths_after_refactor.sql
@@ -12,3 +12,10 @@ UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = replace(CONF_VALUE, '/visualizer/
 -- Se puede ejecutar previamente esta consulta para ver que se va a sustituir lo que deseamos
 -- SELECT CONF_KEY, CONF_VALUE, replace(CONF_VALUE, '/portal/', '/organisations/istac/portal/') FROM TB_DATA_CONFIGURATIONS WHERE CONF_KEY LIKE 'metamac.portal.%.style.%';
 UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = replace(CONF_VALUE, '/portal/', '/organisations/istac/portal/') WHERE CONF_KEY LIKE 'metamac.portal.%.style.%';
+
+
+-- Se puede ejecutar previamente esta consulta para ver que se va a sustituir lo que deseamos
+-- SELECT CONF_KEY, CONF_VALUE, replace(CONF_VALUE, '/apis/', '/organisations/istac/apis/header/') FROM TB_DATA_CONFIGURATIONS WHERE CONF_KEY LIKE 'metamac.api.style.css.url';
+-- SELECT CONF_KEY, CONF_VALUE, replace(CONF_VALUE, '/apis/', '/organisations/istac/apis/header/') FROM TB_DATA_CONFIGURATIONS WHERE CONF_KEY LIKE 'metamac.api.style.header.url';
+UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = replace(CONF_VALUE, '/apis/', '/organisations/istac/apis/header/') WHERE CONF_KEY LIKE 'metamac.api.style.css.url';
+UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = replace(CONF_VALUE, '/apis/', '/organisations/istac/apis/header/') WHERE CONF_KEY LIKE 'metamac.api.style.header.url';
