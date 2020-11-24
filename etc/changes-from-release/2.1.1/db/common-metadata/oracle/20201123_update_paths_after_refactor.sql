7b096c5841f224f171617d7ceba96b4709708a4d
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2020-11-24 11:14:30 +0000

    EDATOS-3215 - Refactorización del código para que la propiedad organización sea escalable
    > corregido error en sql

--- etc/changes-from-release/2.1.1/db/common-metadata/oracle/20201123_update_paths_after_refactor.sql
+++ etc/changes-from-release/2.1.1/db/common-metadata/oracle/20201123_update_paths_after_refactor.sql
@@ -4,9 +4,8 @@
 
 -- Normalizamos las cabeceras del portal de agricultura para que cuelguen del mismo sitio que las demás
 -- Se puede ejecutar previamente esta consulta para ver que se va a sustituir lo que deseamos
--- SELECT CONF_KEY, CONF_VALUE, replace(CONF_VALUE, '/visualizer/agricultura/', '/portal/agriculture/') 
-FROM TB_DATA_CONFIGURATIONS WHERE CONF_KEY LIKE 'metamac.portal.agriculture.style.%';
-UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = replace(CONF_VALUE, '/visualizer/agricultura/', '/portal/agriculture/') WHERE CONF_KEY LIKE 'metamac.portal.%.style.%';
+-- SELECT CONF_KEY, CONF_VALUE, replace(CONF_VALUE, '/visualizer/agricultura/', '/portal/agriculture/') FROM TB_DATA_CONFIGURATIONS WHERE CONF_KEY LIKE 'metamac.portal.agriculture.style.%';
+UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = replace(CONF_VALUE, '/visualizer/agricultura/', '/portal/agriculture/') WHERE CONF_KEY LIKE 'metamac.portal.agriculture.style.%';
 
 -- Movemos las rutas afectadas por el refactor, desde /portal/ hasta /organisations/istac/portal/
 -- Se puede ejecutar previamente esta consulta para ver que se va a sustituir lo que deseamos
