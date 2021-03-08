b32ae310debfa04ca2c9854778592a64681c57e1
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2021-03-08 14:17:40 +0000

    EDATOS-3215 - Refactorización del código para que la propiedad organización sea escalable
    > Añadiendo la propiedad metamac.app.style.favicon.url

---
+++ etc/changes-from-release/3.1.0/db/common-metadata/mssql/20200308_insert_favicon_url_configuration.sql
@@ -0,0 +1,14 @@
+﻿-- --------------------------------------------------------------------------------------------------
+-- EDATOS-3215 - Refactorización del código para que la propiedad organización sea escalable
+-- --------------------------------------------------------------------------------------------------
+
+-- Añadir propiedad con la URL correspondiente al favicon usado en aplicaciones públicas
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.app.style.favicon.url', 'FILL_ME', 1);
+-- Ejemplo DEMO: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.app.style.favicon.url', 'https://estadisticas.arte-consultores.com/apps/organisations/istac/common/favicon.ico', 1);
+-- Ejemplo PRE IBESTAT: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.app.style.favicon.url', 'https://pre-ibestat.edatos.io/apps/organisations/istac/common/favicon.ico', 1);
+-- Ejemplo PRO IBESTAT: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.app.style.favicon.url', 'https://ibestat.edatos.io/apps/organisations/istac/common/favicon.ico', 1);
+-- Ejemplo PRE ISTAC: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.app.style.favicon.url', 'https://www3-pre.gobiernodecanarias.org/aplicaciones/appsistac/organisations/istac/common/favicon.ico', 1);
+-- Ejemplo PRO ISTAC: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.app.style.favicon.url', 'https://www3.gobiernodecanarias.org/aplicaciones/appsistac/organisations/istac/common/favicon.ico', 1);
+UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
+
+commit;
---
+++ etc/changes-from-release/3.1.0/db/common-metadata/postgresql/20200308_insert_favicon_url_configuration.sql
@@ -0,0 +1,14 @@
+﻿-- --------------------------------------------------------------------------------------------------
+-- EDATOS-3215 - Refactorización del código para que la propiedad organización sea escalable
+-- --------------------------------------------------------------------------------------------------
+
+-- Añadir propiedad con la URL correspondiente al favicon usado en aplicaciones públicas
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'), 1, 1, 'metamac.app.style.favicon.url', 'FILL_ME', true);
+-- Ejemplo DEMO: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'), 1, 1, 'metamac.app.style.favicon.url', 'https://estadisticas.arte-consultores.com/apps/organisations/istac/common/favicon.ico', true);
+-- Ejemplo PRE IBESTAT: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'), 1, 1, 'metamac.app.style.favicon.url', 'https://pre-ibestat.edatos.io/apps/organisations/istac/common/favicon.ico', true);
+-- Ejemplo PRO IBESTAT: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'), 1, 1, 'metamac.app.style.favicon.url', 'https://ibestat.edatos.io/apps/organisations/istac/common/favicon.ico', true);
+-- Ejemplo PRE ISTAC: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'), 1, 1, 'metamac.app.style.favicon.url', 'https://www3-pre.gobiernodecanarias.org/aplicaciones/appsistac/organisations/istac/common/favicon.ico', true);
+-- Ejemplo PRO ISTAC: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'), 1, 1, 'metamac.app.style.favicon.url', 'https://www3.gobiernodecanarias.org/aplicaciones/appsistac/organisations/istac/common/favicon.ico', true);
+UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
+
+commit;
